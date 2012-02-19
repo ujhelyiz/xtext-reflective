62d8f219df83e2d7d9dff96e37e7d81c4fa1d7e3
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2012-02-19 20:27:25 +0100

    Re-added support for opening Xtext files with the reflective editor

---
+++ hu.cubussapiens.xtext.reflectiveeditor/src/hu/cubussapiens/xtext/reflectiveeditor/factories/XtextReflectiveExecutableExtensionFactory.java
@@ -0,0 +1,8 @@
+package hu.cubussapiens.xtext.reflectiveeditor.factories;
+
+import org.eclipse.xtext.ui.XtextExecutableExtensionFactory;
+
+public class XtextReflectiveExecutableExtensionFactory extends
+		XtextExecutableExtensionFactory {
+
+}

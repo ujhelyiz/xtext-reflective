161f115136b193e2147b41d4bb1bcaa0ea772d87
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2013-06-26 21:43:14 +0200

    Compatibility change for EMF 2.9 #2

--- hu.cubussapiens.xtext.reflectiveeditor/src/hu/cubussapiens/xtext/reflectiveeditor/editor/ReflectiveXtextEditor.java
+++ hu.cubussapiens.xtext.reflectiveeditor/src/hu/cubussapiens/xtext/reflectiveeditor/editor/ReflectiveXtextEditor.java
@@ -1,26 +1,16 @@
 package hu.cubussapiens.xtext.reflectiveeditor.editor;
 
-import java.util.EventObject;
-
 import org.eclipse.core.resources.IFile;
-import org.eclipse.emf.common.command.BasicCommandStack;
-import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.command.CommandStack;
-import org.eclipse.emf.common.command.CommandStackListener;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.presentation.EcoreEditor;
-import org.eclipse.emf.ecore.provider.EcoreItemProviderAdapterFactory;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
-import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
-import org.eclipse.emf.edit.provider.ReflectiveItemProviderAdapterFactory;
-import org.eclipse.emf.edit.provider.resource.ResourceItemProviderAdapterFactory;
 import org.eclipse.emf.edit.ui.util.EditUIUtil;
 import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.xtext.Constants;
 import org.eclipse.xtext.resource.IResourceFactory;
@@ -117,49 +107,11 @@ public class ReflectiveXtextEditor extends EcoreEditor {
 
 	@Override
 	protected void initializeEditingDomain() {
-		// Create an adapter factory that yields item providers.
-		//
-		adapterFactory = new ComposedAdapterFactory(
-				ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
-
-		adapterFactory
-				.addAdapterFactory(new ResourceItemProviderAdapterFactory());
-		adapterFactory.addAdapterFactory(new EcoreItemProviderAdapterFactory());
-		adapterFactory
-				.addAdapterFactory(new ReflectiveItemProviderAdapterFactory());
-
-		// Create the command stack that will notify this editor as commands are
-		// executed.
-		//
-		BasicCommandStack commandStack = new BasicCommandStack();
-
-		// Add a listener to set the most recent command's affected objects to
-		// be the selection of the viewer with focus.
-		//
-		commandStack.addCommandStackListener(new CommandStackListener() {
-			public void commandStackChanged(final EventObject event) {
-				getContainer().getDisplay().asyncExec(new Runnable() {
-					public void run() {
-						firePropertyChange(IEditorPart.PROP_DIRTY);
-
-						// Try to select the affected objects.
-						//
-						Command mostRecentCommand = ((CommandStack) event
-								.getSource()).getMostRecentCommand();
-						if (mostRecentCommand != null) {
-							setSelectionToViewer(mostRecentCommand
-									.getAffectedObjects());
-						}
-						if (propertySheetPage != null
-								&& !propertySheetPage.getControl().isDisposed()) {
-							propertySheetPage.refresh();
-						}
-					}
-				});
-			}
-		});
+        super.initializeEditingDomain();
 		// The next line is changed to load the editing domain from a resource
 		// set
+        final CommandStack commandStack = editingDomain.getCommandStack();
+        // although an editingDomain is created in the parent, it will not operate correctly
 		editingDomain = new AdapterFactoryEditingDomain(adapterFactory,
 				commandStack, resourceSet);
 	}
